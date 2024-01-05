28b7b5f4e1a81cc7274373c90a4f1bc4836122ef
isurukumanayake <kumanayake.isuru@gmail.com>
2024-01-05 22:48:37 +0530

    Updates

---
+++ src/events/order-cancelled-event.ts
@@ -0,0 +1,14 @@
+import { Subjects } from "./subjects";
+
+export interface OrderCancelledEvent {
+  // The subject property is used by the NATS Streaming Server to route events to the correct subscriber.
+  // It is a string value that must be one of the values from the Subjects enum.
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    version: number;
+    ticket: {
+      id: string;
+    };
+  };
+}
---
+++ src/events/order-created-event.ts
@@ -0,0 +1,18 @@
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
+
+export interface OrderCreatedEvent {
+  // The subject property is used by the NATS Streaming Server to route events to the correct subscriber.
+  // It is a string value that must be one of the values from the Subjects enum.
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
